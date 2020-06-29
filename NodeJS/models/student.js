7516e5397d1950db19fa82193b96a11cad33da4e
Tshweu <t.t.sephiri@gmail.com>
2020-06-30 00:58:01 +0200

    Updated student.js

--- NodeJS/models/student.js
+++ NodeJS/models/student.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
-    StudentNo : String,
-    Password : String,
+    studentNo : String,
+    password : String,
     classes : []
 })
 
-const StudentModel = mongoose.model('student',StudentSchema); 
-module.exports = StudentModel;
+const Student = mongoose.model('student',StudentSchema); 
+module.exports = Student;
