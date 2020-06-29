4b2328b507a474f690963869a91131cb01bb8ab1
Tshweu <t.t.sephiri@gmail.com>
2020-06-30 00:59:55 +0200

    Added connection to database

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -7,6 +7,19 @@ var port = process.env.PORT || "8000";
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json());
 
+//Making mongodb connection 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+    mongoose.connect('mongodb://localhost/Timetable2020');
+
+    mongoose.connection.once('open',function(){
+        console.log('Connection has been successfully made');
+    }).on('error',function(error) {
+        console.log('There is an error ',error);
+    });
+
+
 
 
 app.listen(port,()=>{
