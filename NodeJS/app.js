0fe7d32fbc75caa45586bc30307f8e363cb244c2
Tshweu <t.t.sephiri@gmail.com>
2020-06-30 03:43:41 +0200

    added basic student info update endpoint

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 var port = process.env.PORT || "8000";
 
 
+
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(bodyParser.json());
 
 //Making mongodb connection 
 const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
     mongoose.connect('mongodb://localhost/Timetable2020');
@@ -22,11 +24,22 @@ mongoose.Promise = global.Promise;
 
 //making a post request using student model
 const Student = require('./models/student');    
-app.post('/newStudent',(req,res,error)=>{
+app.post('/newStudent',(req,res,next)=>{
     Student.create(req.body).then((student)=>{
         res.send(student);
-    }).catch(error);
+    }).catch(next);
 })
+
+//updating student class info
+app.put('/updateStudent',(req,res,next)=>{
+    Student.findByIdAndUpdate({_id:req.body._id},req.body).then((student)=>{
+        res.send(student);
+    }).catch(next);
+})
+
+
+
+//Delete all classes
 
 app.listen(port,()=>{
     console.log('server is running on '+[port]);
