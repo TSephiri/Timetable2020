db4b357f8cf2a5ac36ee8e904c89f0e704e095d8
Tshweu <t.t.sephiri@gmail.com>
2020-08-27 16:59:47 +0200

    Fixed student update

--- NodeJS/controllers/studentController.js
+++ NodeJS/controllers/studentController.js
@@ -7,12 +7,12 @@ var ObjectId = require('mongoose').Types.ObjectId;
 var  StudentModel  = require('../models/student');
 var  ClassModel  = require('../models/class')
 
-router.get('/', (req, res) => {
-    StudentModel.find((err, docs) => {
+router.get('/:id', (req, res) => {
+    StudentModel.findOne({StudentNo:req.params.id},(err, docs) => {
         if (!err) {
             res.send(docs);
         } else {
-            console.log("Error at retrieving students: " + JSON.stringify(err, undefined, 2));
+            console.log("Error at retrieving student: " + JSON.stringify(err, undefined, 2));
         }
     })
 });
@@ -33,28 +33,53 @@ router.post('/', (req, res,next) => {
     }).catch(next)
 })
 
-router.put('/:id', (req, res) => {
-    //check if given class id exists
-    if (!ObjectId.isValid(req.params.id)) {
-        return res.status(400).send(`No record with given id: ${req.params.id}`);
+//**************** */
+
+router.put('/:id',(req,res,next)=>{
+    //find student
+    var newInfo = {
+        degree_id: req.body.Degree_id,
+        module: req.body.module,
+        day: req.body.day,
+        time: req.body.time,
+        venue: req.body.venue
     }
 
-    var student = {
-        StudentNo: req.body.StudentNo,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Degree_id: req.body.Degree_id,
-
-    };
-    StudentModel.findByIdAndUpdate(req.params.id, { $set: student }, { new: true }, (err, docs) => {
-        if (!err) {
-            res.send(docs)
-        } else {
-            console.log("Error at updating student: " + JSON.stringify(err, undefined, 2))
-        }
-    })
-
+    StudentModel.findOne({StudentNo:req.params.id},(err,docs)=>{      
+        console.log(docs._id);
+        var id = docs._id;
+        StudentModel.findOne({
+            _id: id, 'classes.time': req.body.time,
+            'classes.day': req.body.day
+        }, (err, docs) => {
+            if (docs) {
+                StudentModel.findOneAndUpdate({_id:id,'classes.time':req.body.time,
+                    'classes.day': req.body.day
+                }, { 'classes.$': newInfo }
+                    , (error, docs) => {
+                        if (!error) {
+                            res.send("Updated student");
+                            console.log(docs);
+                        } else {
+                            res.send(error);
+                        }
+                    }).catch(next)
+            } else {
+                StudentModel.updateOne({ _id: id }, { '$push': { 'classes': newInfo } },
+                    (error, docs) => {
+                        if (!error) {
+                            res.send("Updated student");
+                            console.log(docs);
+                        } else {
+                            console.log(error);
+                            res.send(error);
+                        }
+                     })
+            }
+        })
+    })   
 })
+            
 
 //Delete
 router.delete('/:id', (req, res) => {
@@ -72,8 +97,5 @@ router.delete('/:id', (req, res) => {
     })
 });
 
-var getDegreeInfo = function(degree) {
-    
-}
 
 module.exports = router;
