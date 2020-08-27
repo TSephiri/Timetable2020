0c6920ba5ec52b18e5cf468c19b7d9e1942a16e1
Tshweu <t.t.sephiri@gmail.com>
2020-08-27 17:01:36 +0200

    Removed put to update student

--- NodeJS/controllers/studentController.js
+++ NodeJS/controllers/studentController.js
@@ -32,55 +32,7 @@ router.post('/', (req, res,next) => {
         })
     }).catch(next)
 })
-
-//**************** */
-
-router.put('/:id',(req,res,next)=>{
-    //find student
-    var newInfo = {
-        degree_id: req.body.Degree_id,
-        module: req.body.module,
-        day: req.body.day,
-        time: req.body.time,
-        venue: req.body.venue
-    }
-
-    StudentModel.findOne({StudentNo:req.params.id},(err,docs)=>{      
-        console.log(docs._id);
-        var id = docs._id;
-        StudentModel.findOne({
-            _id: id, 'classes.time': req.body.time,
-            'classes.day': req.body.day
-        }, (err, docs) => {
-            if (docs) {
-                StudentModel.findOneAndUpdate({_id:id,'classes.time':req.body.time,
-                    'classes.day': req.body.day
-                }, { 'classes.$': newInfo }
-                    , (error, docs) => {
-                        if (!error) {
-                            res.send("Updated student");
-                            console.log(docs);
-                        } else {
-                            res.send(error);
-                        }
-                    }).catch(next)
-            } else {
-                StudentModel.updateOne({ _id: id }, { '$push': { 'classes': newInfo } },
-                    (error, docs) => {
-                        if (!error) {
-                            res.send("Updated student");
-                            console.log(docs);
-                        } else {
-                            console.log(error);
-                            res.send(error);
-                        }
-                     })
-            }
-        })
-    })   
-})
             
-
 //Delete
 router.delete('/:id', (req, res) => {
     //check if given class id exists
