9ea8142c7215205a8a14fe52124a0fc58283cc25
Tshweu <t.t.sephiri@gmail.com>
2020-08-27 23:04:08 +0200

    API call to update schedule based on given info

--- app/src/main/java/com/example/app/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/app/Retrofit/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit instance_Post;
-    private static String url =  "http://192.168.8.105:3000/";//API ip address
+    private static String url =  "http://192.168.43.224:3000/";//API ip address
 
         // "http://192.168.43.224:3000/";
         //"http://192.168.8.105:3000/";//API ip address
--- app/src/main/java/com/example/app/UpdateInfo.java
+++ app/src/main/java/com/example/app/UpdateInfo.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.app.Retrofit.NodeJS;
+import com.example.app.Retrofit.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class UpdateInfo extends AppCompatActivity {
 
     String module,time,venue,day;
     EditText eModule,eTime,eVenue,eDay;
+    NodeJS myAPI;
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,14 @@ public class UpdateInfo extends AppCompatActivity {
         eTime = findViewById(R.id.time);
         eVenue = findViewById(R.id.venue);
         Button saveInfo = findViewById(R.id.updateInfo);
+
+        Intent intent = getIntent();
+        eTime.setText(getTime(intent.getStringExtra("TimeSlot")));
+        eDay.setText(intent.getStringExtra("Day"));
+
+        retrofit = RetrofitClient.getInstance();
+        myAPI = retrofit.create(NodeJS.class);
+
         saveInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,10 +53,75 @@ public class UpdateInfo extends AppCompatActivity {
                 //day of the week should also be from the fragment
                 day = eDay.getText().toString();
 
-                startActivity(new Intent(UpdateInfo.this,MainActivity.class));
-                finish();
+                update();
+
             }
         });
 
+    }
+    //Change student number to a parameter pass from each class
+    public void update(){
+        Call<String> call = myAPI.updateClasses("12345678",module,time,venue,day);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if(response.isSuccessful()){
+                    Log.i("","Updated Student ");
+                    Toast.makeText(UpdateInfo.this,"Saved Info",Toast.LENGTH_SHORT);
+                    Intent newIntent = new Intent(UpdateInfo.this,MainActivity.class);
+                    startActivity(newIntent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                if(call.isCanceled()) {
+                    Log.e("", "request was aborted");
+                }else {
+                    Log.e("", "Unable to submit post to API.");
+                }
+                //showErrorMessage();
+
+            }
+        });
+    }
+    //convert position to time
+    public String getTime(String position){
+        String time = " ";
+        switch (position)
+        {
+            case "0":
+             time = "8:00";
+                break;
+            case "1":
+                time = "9:00";
+                break;
+            case "2":
+                time = "10:00";
+                break;
+            case "3":
+                time = "11:00";
+                break;
+            case "4":
+                time = "12:00";
+                break;
+            case "5":
+                time = "13:00";
+                break;
+            case "6":
+                time = "14:00";
+                break;
+            case "7":
+                time = "15:00";
+                break;
+            case "8":
+                time = "16:00";
+                break;
+            case "9":
+                time = "17:00";
+                break;
+        }
+        return time;
     }
 }
